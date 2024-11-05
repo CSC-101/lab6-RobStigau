bf0a5bd14b4351006c3091c3687c36caaa592d00
RobTigau <rtigau@calpoly.edu>
2024-11-04 17:24:07 -0800

    completed lab 6

--- lab6.py
+++ lab6.py
@@ -37,6 +37,68 @@ def selection_sort(values:list[int]) -> None:
 
 
 # Part 1
+def selection_sort_books(book_list : list[data.Book]):
+    final_book_list = []
+    book_dictionary = {}
+    if len(book_list) == 0:
+        return None
+    for book in book_list:
+        book_dictionary[book.title] = book
+    sorted_list = sorted(book_dictionary)
+    for item in sorted_list:
+        for book in book_list:
+            if item == book.title:
+                final_book_list.append(book)
+            else:
+                continue
+    return final_book_list
+
+
+def swap_case(string : str) -> str:
+    new_string_list = []
+    string_list = list(string)
+    for item in string_list:
+        if ord(item) < 65 and ord(item) > 90 and ord(item) < 97 and ord(item) > 122:
+            continue
+        if item.islower() == True:
+            item = item.upper()
+            new_string_list.append(item)
+        else:
+            item = item.lower()
+            new_string_list.append(item)
+    new_string = "".join(new_string_list)
+    return new_string
+strings = '#sigMA1'
+print(swap_case(strings))
+
+def str_translate(string : str, old : str, new : str):
+    new_string_list = []
+    string_list = list(string)
+    for item in string_list:
+        if item == old:
+            new_string_list.append(new)
+        else:
+            new_string_list.append(item)
+    new_string = "".join(new_string_list)
+    return new_string
+print(str_translate('sigmas', 's', 'o'))
+
+def histogram(paragraph : str):
+    paragraph_list = paragraph.split()
+    word_dictionary = {}
+    for i in paragraph_list:
+        if i not in word_dictionary:
+            word_dictionary[i] = 1
+        else:
+            word_dictionary[i] += 1
+    return word_dictionary
+
+
+
+str = "The people who are crazy enough to think they can change the world are the ones who do"
+print(histogram(str))
+
+
 
 
 # Part 2
